36ac05851549ce814330d005ff1a3cd2032eb5b7
smiling-sky <70390276+smiling-sky@users.noreply.github.com>
2020-10-16 21:42:38 +0300

    вроде все сделал

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,40 @@
 const button = document.querySelector(".button-offer");
-const modal = document.querySelector(".modal")
+const modal = document.querySelector(".modal");
+const arrival = document.querySelector(".input-arrival-date");
+const departure = document.querySelector(".input-of-departure");
+const formInput = document.querySelector(".search-hotel-form");
+const inputAdults = document.querySelector(".input-adults");
+const inputChildren = document.querySelector(".input-children");
 
-button.addEventListener("click", function (evt){
+let storageAdultsSupport = true;
+let storageAdults = "";
+let storageChildrenSupport = true;
+let storageChildren = "";
+
+try {
+  storageAdults = localStorage.getItem("adults");
+  storageChildren = localStorage.getItem("children");
+} catch (err) {
+  storageAdultsSupport = false;
+  storageChildrenSupport = false;
+}
+
+button.addEventListener("click", function (evt) {
+  console.log("клик по поиску гостиницы");
+  evt.preventDefault();
+  modal.classList.toggle("modal-hide");
+});
+
+formInput.addEventListener("submit", function (evt) {
+  if (!departure.value || !arrival.value || !inputAdults.value || !inputChildren.value) {
     evt.preventDefault();
-    modal.classList.toggle("modal-hide");
+    console.log(" Нужно заполнить все поля ");
+  } else {
+    if (storageAdultsSupport) {
+      localStorage.setItem("adults", inputAdults.value);
+    }
+    if (storageChildrenSupport) {
+      localStorage.setItem("children", inputChildren.value);
+    }
+  }
 });
